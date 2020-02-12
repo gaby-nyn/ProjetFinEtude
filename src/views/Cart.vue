e950a3dff5cc329fa0afdf0b09ed42f9743bf777
Gaby <gaby.nyn@gmail.com>
2020-02-12 15:07:24 -0500

    Added address form, verification missing for it

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,11 +1,10 @@
 <template>
     <div>
-        <h1 class="title">Enter credit card information here</h1>
         <CheckoutForm></CheckoutForm>
     </div>
 </template>
 <script>
-import CreditCardForm from '@/components/CheckoutForm.vue'
+import CheckoutForm from '@/components/CheckoutForm.vue'
 export default {
     name: 'Cart',
     components: {
