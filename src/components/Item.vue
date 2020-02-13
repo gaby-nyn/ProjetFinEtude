1311f7d2c6e13a294f5c048b37a9c6b423cbab12
Gaby <gaby.nyn@gmail.com>
2020-02-13 18:41:47 -0500

    Temporary item ui, test placement when displayed

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,8 +1,31 @@
 <template>
-    <div>
-        <img src="" alt="">
-        <h1>This is the name</h1>
-        <h2>This is the description</h2>\
-        <h1>id, description, type, marque, taille, prix, inventaire(quantite), photo, selectionne(enlever), categorie, commande, commandeproduit</h1>
-    </div>
+<div class="tile is-parent">
+    <article class="tile is-child notification is-info is-box">
+        <figure class="image is-4by3">
+        <img src="https://bulma.io/images/placeholders/640x480.png">
+        </figure>
+        <p class="title">{{ Name }}</p>
+        <p class="subtitle">{{ Description }}</p>
+    </article>
+</div>
 </template>
+<script>
+// import axios from 'axios';
+export default {
+    data() {
+        return {
+            Item: {},
+            Name: 'AF1',
+            Description: 'This is the description',
+            Type: '',
+            Marque: '',
+            Taille: null,
+            Prix: null,
+            Inventory: null,
+            Photo: '',
+            Selected: null,
+            Category: '',
+        };
+    }
+}
+</script>
--- src/views/MenCategory.vue
+++ src/views/MenCategory.vue
@@ -1,71 +1,93 @@
 <template>
 <div>
     <h1 class="title">Men's</h1>
-    <div name="FilterBrand">
-        <h2 class="subtitle">Brands</h2>
-        <div>        
-            <label class="radio">
-                <input type="radio" name="Brand">
-                Nike
-            </label>
+    <div class="columns">
+        <div class="column is-one-fifth">
+            <div name="FilterBrand">
+                <h2 class="subtitle">Brands</h2>
+                <div>        
+                    <label class="radio">
+                        <input type="radio" name="Brand">
+                        Nike
+                    </label>
+                </div>
+                <div>
+                    <label class="radio">
+                        <input type="radio" name="Brand">
+                        Adidas
+                    </label>
+                </div>
+                <div>
+                    <label class="radio">
+                        <input type="radio" name="Brand">
+                        Puma
+                    </label>
+                </div>
+                <div>
+                    <label class="radio">
+                        <input type="radio" name="Brand">
+                        Under Armour
+                    </label>                        
+                </div>
+            </div>
+            <div name="FilterShoe">
+                <h2 class="subtitle">Shoe type</h2>
+                <div>        
+                    <label class="radio">
+                        <input type="radio" name="Brand">
+                        Sandale
+                    </label>
+                </div>
+                <div>
+                    <label class="radio">
+                        <input type="radio" name="Brand">
+                        Botte
+                    </label>
+                </div>
+                <div>
+                    <label class="radio">
+                        <input type="radio" name="Brand">
+                        Sport
+                    </label>
+                </div>
+                <div>
+                    <label class="radio">
+                        <input type="radio" name="Brand">
+                        Casual
+                    </label>                        
+                </div>
+                <div>
+                    <label class="radio">
+                        <input type="radio" name="Brand">
+                        Formal
+                    </label>                        
+                </div>        
+            </div>
         </div>
-        <div>
-            <label class="radio">
-                <input type="radio" name="Brand">
-                Adidas
-            </label>
-        </div>
-        <div>
-            <label class="radio">
-                <input type="radio" name="Brand">
-                Puma
-            </label>
-        </div>
-        <div>
-            <label class="radio">
-                <input type="radio" name="Brand">
-                Under Armour
-            </label>                        
-        </div>
-    </div>
-    <div name="FilterShoe">
-        <h2 class="subtitle">Shoe type</h2>
-        <div>        
-            <label class="radio">
-                <input type="radio" name="Brand">
-                Sandale
-            </label>
-        </div>
-        <div>
-            <label class="radio">
-                <input type="radio" name="Brand">
-                Botte
-            </label>
-        </div>
-        <div>
-            <label class="radio">
-                <input type="radio" name="Brand">
-                Sport
-            </label>
-        </div>
-        <div>
-            <label class="radio">
-                <input type="radio" name="Brand">
-                Casual
-            </label>                        
-        </div>
-        <div>
-            <label class="radio">
-                <input type="radio" name="Brand">
-                Formal
-            </label>                        
+        <div class="column is-one-quarter is-horizontal">                        
+            <Item class="item"></Item>
+            <Item class="item"></Item>
+            <Item class="item"></Item>
+            <Item class="item"></Item>
         </div>        
     </div>
 </div>
-
 </template>
+
+<script>
+import Item from '@/components/Item.vue'
+export default {
+    components: {
+        Item,
+    }
+}
+</script>
+
 <style scoped>
 div [name="FilterBrand"] {
     padding-bottom: 50px;
+}
+.item {
+    padding-left: 20px;
 }
 </style>
